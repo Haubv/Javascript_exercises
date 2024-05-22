3b04e9e7c2076de7aa07e2e51752b31a97c48134
Bùi Văn Hậu <HauBV@vissoft.vn>
2024-05-22 16:35:30 +0700

    modify

--- script.js
+++ script.js
@@ -58,7 +58,6 @@ function GetMax() {
     
     if (Math.floor(number1) - number1 !=0 || Math.floor(number2) - number2 !=0 || Math.floor(number3) - number3 !=0 || isNaN(number1) || isNaN(number2) || isNaN(number3)) {
         alert("Không phải số nguyên, vui lòng nhập lại.")
-        window.location.reload()
     }
     let max = Math.max(number1, number2, number3)
     document.getElementById("ex4.display").innerText = "Số lớn nhất trong 3 số " + 
